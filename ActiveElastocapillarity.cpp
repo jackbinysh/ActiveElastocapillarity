ea6de7f0370ae67a6947cc5310e46ee24431fb1f
jackbinysh <jack.binysh@gmail.com>
2020-09-29 11:53:38 +0100

    more tinkering

--- ActiveElastocapillarity.cpp
+++ ActiveElastocapillarity.cpp
@@ -13,18 +13,83 @@ using namespace std;
 int main(int argc,char *argv[])
 {
 
+  int system_size=6;
+  string fileName = "TestMesh.vtu";
+  const ui dim=3;
+
   // Initialise the MD simulation
-	//md<2> sys(system_size);
-	//sys.index();
-	//sys.network.update = false;
+	md<dim> sys(system_size);
+	sys.index();
+	sys.network.update = false;
+  
 
   // Read in the mesh
-  string fileName = "TestMesh.vtu";
-  vtkSmartPointer<vtkUnstructuredGrid> InputMesh;
   auto reader = vtkSmartPointer<vtkXMLUnstructuredGridReader>::New();
   reader->SetFileName (fileName.c_str());
   reader->Update();
-  InputMesh = reader->GetOutput();
+  auto InputMesh = reader->GetOutput();
+
+  // tell the MD sim about particle positions
+  for (vtkIdType i = 0; i < InputMesh->GetNumberOfPoints() ; i++)
+  {
+    double* x;
+    x=InputMesh->GetPoint(i);
+
+    if(2==dim)
+    {
+      sys.import_pos(i,x[0],x[1]);
+    }
+    else if(3==dim)
+    {
+      sys.import_pos(i,x[0],x[1],x[2]);
+    }
+  }
+
+  // tell the MD sim about particle bonds
+  auto cellArray = InputMesh->GetCells();
+  cellArray->InitTraversal();
+  vtkIdType npts, *pts;
+  while(cellArray->GetNextCell(npts, pts))
+  {
+    if(2==npts) // i.e if the cell is an edge
+    {
+      //cout <<pts[0] << "\t" <<pts[1] << "\n";
+      bool made = sys.add_bond(pts[0], pts[1], POT::HOOKEAN); //add spring, neohookean potential
+      cout << made;
+    }
+  }
+
+  // Run the sim
+  ui run_time = 1000000;
+	ldf time_step = 1E-7; 
+	sys.integrator.h = time_step;
+	for (ui i = 0; i < run_time; i++) 
+  {
+		sys.timestep(); 
+  }
+  
+  //output
+	for (ui i = 0; i < sys.particles.size(); i++) 
+  {
+    ldf x[3];
+    x[2]=0;
+    for(int d=0;d<dim;d++)
+    {
+      x[d]=sys.particles[i].x[d];
+      cout <<x[d] << "\t";
+    }
+    InputMesh->GetPoints()->SetPoint(i,x);
+  }
+  string outputname = "out.vtu";
+  auto writer = vtkSmartPointer<vtkXMLUnstructuredGridWriter>::New();
+  writer->SetFileName(outputname.c_str());
+  writer->SetInputData(InputMesh);
+  writer->Write();
+
+
+
+  //  cout <<npts << endl;
+  //  cout <<pts[0] << "\t" << pts[1] << "\t" << endl;
 
   /*
   auto cellArray = InputMesh->GetCells();
@@ -60,6 +125,7 @@ int main(int argc,char *argv[])
    }
    */
 
+  /*
   auto cellArray = InputMesh->GetCellData();
   //cout << cellArray->GetClassName();
   //InputMesh->GetCellData()->GetScalars();
@@ -69,6 +135,7 @@ int main(int argc,char *argv[])
        double* x= InputMesh->GetCellData()->GetScalars()->GetTuple(i);
        cout << *x <<endl;
    }
+   */
 
 
 /*
