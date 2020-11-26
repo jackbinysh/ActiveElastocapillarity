e1a0a41557e901580f20641c3ac4ab8ce1b09a45
jackbinysh <jack.binysh@gmail.com>
2020-11-26 16:15:42 +0000

    Adding ellipsoid fitting code

--- Python/EnergyMinimization/AnalysisFunctions.py
+++ Python/EnergyMinimization/AnalysisFunctions.py
@@ -91,3 +91,79 @@ def fit_ellipse(x, y):
     # ensure the angle is betwen 0 and 2*pi
     phi -= 2 * np.pi * int(phi / (2 * np.pi))
     return [M, m, centre[0], centre[1], phi]
+
+
+# Adapted from http://www.juddzone.com/ALGORITHMS/least_squares_3D_ellipsoid.html
+#least squares fit to a 3D-ellipsoid
+#  Ax^2 + By^2 + Cz^2 +  Dxy +  Exz +  Fyz +  Gx +  Hy +  Iz  = 1
+#
+# Note that sometimes it is expressed as a solution to
+#  Ax^2 + By^2 + Cz^2 + 2Dxy + 2Exz + 2Fyz + 2Gx + 2Hy + 2Iz  = 1
+# where the last six terms have a factor of 2 in them
+# This is in anticipation of forming a matrix with the polynomial coefficients.
+# Those terms with factors of 2 are all off diagonal elements.  These contribute
+# two terms when multiplied out (symmetric) so would need to be divided by two
+def ls_ellipsoid(xx,yy,zz):
+
+   # change xx from vector of length N to Nx1 matrix so we can use hstack
+    x = xx[:,np.newaxis]
+    y = yy[:,np.newaxis]
+    z = zz[:,np.newaxis]
+
+    #  Ax^2 + By^2 + Cz^2 +  Dxy +  Exz +  Fyz +  Gx +  Hy +  Iz = 1
+    J = np.hstack((x*x,y*y,z*z,x*y,x*z,y*z, x, y, z))
+    K = np.ones_like(x) #column of ones
+
+    #np.hstack performs a loop over all samples and creates
+    #a row in J for each x,y,z sample:
+    # J[ix,0] = x[ix]*x[ix]
+    # J[ix,1] = y[ix]*y[ix]
+    # etc.
+
+    JT=J.transpose()
+    JTJ = np.dot(JT,J)
+    InvJTJ=np.linalg.inv(JTJ);
+    ABC= np.dot(InvJTJ, np.dot(JT,K))
+
+    # Rearrange, move the 1 to the other side
+    #  Ax^2 + By^2 + Cz^2 +  Dxy +  Exz +  Fyz +  Gx +  Hy +  Iz - 1 = 0
+    #    or
+    #  Ax^2 + By^2 + Cz^2 +  Dxy +  Exz +  Fyz +  Gx +  Hy +  Iz + J = 0
+    #  where J = -1
+    vec=np.append(ABC,-1)
+    
+    # Now convert this polynomial into a center, axes, etc.
+    
+    Amat=np.array(
+    [
+    [ vec[0],     vec[3]/2.0, vec[4]/2.0, vec[6]/2.0 ],
+    [ vec[3]/2.0, vec[1],     vec[5]/2.0, vec[7]/2.0 ],
+    [ vec[4]/2.0, vec[5]/2.0, vec[2],     vec[8]/2.0 ],
+    [ vec[6]/2.0, vec[7]/2.0, vec[8]/2.0, vec[9]     ]
+    ])
+
+   #See B.Bartoni, Preprint SMU-HEP-10-14 Multi-dimensional Ellipsoidal Fitting
+   # equation 20 for the following method for finding the center
+    A3=Amat[0:3,0:3]
+    A3inv=inv(A3)
+    ofs=vec[6:9]/2.0
+    center=-np.dot(A3inv,ofs)
+
+    # Center the ellipsoid at the origin
+    Tofs=np.eye(4)
+    Tofs[3,0:3]=center
+    R = np.dot(Tofs,np.dot(Amat,Tofs.T))
+
+    R3=R[0:3,0:3]
+    R3test=R3/R3[0,0]
+    s1=-R[3, 3]
+    R3S=R3/s1
+    (el,ec)=eig(R3S)
+
+    recip=1.0/np.abs(el)
+    axes=np.sqrt(recip)
+
+    inve=inv(ec) #inverse is actually the transpose here
+    return (center,axes,ec,inve,vec)
+
+
